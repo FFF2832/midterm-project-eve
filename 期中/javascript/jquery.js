cd76e681d9364da52d6616397290a219313bb6ba
FFF2832 <90552202+FFF2832@users.noreply.github.com>
2021-11-07 21:36:07 +0800

    first commit

---
+++ 期中/javascript/jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.mvpic').slick({
+    // dots: true,
+    slidesToShow: 3,
+        slidesToScroll: 1,
+       autoplay: true,
+       autoplaySpeed: 2000,
+    centerMode: true,
+    arrows: true,
+   
+  });  
+});
+
